4c3e2d8a3619d7f4c56a56bd0e09f24699bbee95
unknown <mohammad.mazharul.hoque7@gmail.com>
2020-03-24 17:43:00 +0600

    bootstrap 4 added and cleaned up app.component

--- angular-practise-app/src/app/app.component.ts
+++ angular-practise-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-practise-app';
+  title = 'Angular-Practise-App';
 }
