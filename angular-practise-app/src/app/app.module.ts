283b91e9f7862c2bf9297f0474432b377d71a43f
unknown <mohammad.mazharul.hoque7@gmail.com>
2020-03-25 23:10:23 +0600

    employee-list and emplpoyee details component added incl employee service

--- angular-practise-app/src/app/app.module.ts
+++ angular-practise-app/src/app/app.module.ts
@@ -6,20 +6,26 @@ import { StringInterpolationComponent } from './string-interpolation/string-inte
 import { BindingsComponent } from './bindings/bindings.component';
 import { DirectivesComponent } from './directives/directives.component';
 import { OthersComponent } from './others/others.component';
-
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeService } from './employee.service';
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
     StringInterpolationComponent,
     BindingsComponent,
     DirectivesComponent,
-    OthersComponent
+    OthersComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular-practise-app/src/app/employee.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private _url = "/assets/data/employee.json"
+  constructor(private http: HttpClient) { }
+
+  getEmployees(){
+    return this.http.get(this._url);
+    
+  }
+}
